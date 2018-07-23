78373c6320968e886899ae3be1582e43be3819eb
Bruno Zaccaria Marques <zaccabruno@gmail.com>
2018-07-23 01:19:59 -0300

    Added url at .properties

--- src/main/java/br/com/pismo/transactions/external/HttpAccountLimitBalancer.java
+++ src/main/java/br/com/pismo/transactions/external/HttpAccountLimitBalancer.java
@@ -4,6 +4,7 @@ import br.com.pismo.transactions.domain.AccountLimitBalancer;
 import br.com.pismo.transactions.domain.LimitType;
 import br.com.pismo.transactions.external.exceptions.AccountNotFoundException;
 import br.com.pismo.transactions.external.exceptions.InsufficientFundsException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,9 @@ public class HttpAccountLimitBalancer implements AccountLimitBalancer {
 
     private HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
     private RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+    @Value("${app.accounts.url}")
+    private String accountsUrl;
 
     @Override
     public void updateLimits(Long accountId, LimitType limitType, BigDecimal amount) {
@@ -39,7 +43,7 @@ public class HttpAccountLimitBalancer implements AccountLimitBalancer {
         HttpEntity<Request> entity = new HttpEntity<>(request, headers);
 
         try {
-            restTemplate.exchange("http://localhost:8081/v1/accounts/" + accountId, HttpMethod.PATCH, entity, Void.class);
+            restTemplate.exchange(accountsUrl + accountId, HttpMethod.PATCH, entity, Void.class);
         } catch (HttpClientErrorException exception) {
             HttpStatus statusCode = exception.getStatusCode();
 
